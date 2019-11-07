ac49626a02414e2d902a02bda0b209820587bd2b
v5591178 <55239491+v5591178@users.noreply.github.com>
2019-11-07 20:57:35 +0800

    Delete q2.ts

--- q2.ts
+++
@@ -1,6 +0,0 @@
-// 題目說明: 請使用套件readline-sync跟node-qrcode，輸入文字來產生QRCODE
-// 輸入說明: 請打上任意文字，並產生QRCODE
-// 輸出說明: 請輸出檔名為QRCODE.png圖片
-
-import * as QRCode from 'qrcode';
-QRCode.toFile('./QRCODE.png','Today is 星期四')
